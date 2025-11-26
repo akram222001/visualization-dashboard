6c97c25b331ad3d8e7258756fbe97230f6411eaa
akram222001 <akramansari222001@gmail.com>
2025-11-26 23:53:50 +0530

    live api test and setup

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const api = {
   // Get data with filters
