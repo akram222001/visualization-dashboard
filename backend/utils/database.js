32730937415a1cc169f13133c016b7d18f3c225d
akram222001 <akramansari222001@gmail.com>
2025-11-26 23:43:13 +0530

    changed code in api

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dashboardDB');
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     console.log(`Database: ${conn.connection.name}`);
   } catch (error) {
